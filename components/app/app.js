be7e15803af2610d150c3fbc88f813204601f821
Artsiom Mezin <iketari@gmail.com>
2017-12-01 20:11:18 +0000

    two components

--- components/app/app.js
+++ components/app/app.js
@@ -1,18 +1,49 @@
 import {Chat} from './../chat/chat.js';
 import {Form} from './../form/form.js';
 
-class App {
+export class App {
     constructor({el}) {
         this.el = el;
-        this.chat = new Chat();
-        this.form = new Form();
+        this.chat = new Chat({
+            el: document.createElement('div')
+        });
+        this.form = new Form({
+            el: document.createElement('div'),
+            onSubmit: this._onFormSubmit.bind(this)
+        });
+
+        this.el.append(this.chat.el, this.form.el);
+        this.chat.setData({
+            messages: [
+                {
+                    sender: 'Ekaterina',
+                    text: 'тогда и класс надо менять'
+                },
+                {
+                    sender: 'Ivan',
+                    text: 'закрывающий div'
+                }
+            ]
+        });
 
         this.render();
     }
 
     render() {
-        this.el.innerHTML = `
-            <h3>SPA!</h3>  
-        `;
+        this.chat.render();
+        this.form.render();
+    }
+
+    _onFormSubmit({text}) {
+        this.chat.setData({
+            messages: [
+                ...this.chat.data.messages,
+                {
+                    text,
+                    sender: 'Me'
+                }
+            ]
+        });
+        this.render();
     }
 }
